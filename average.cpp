5dd1016c3fcc5cf39aa206aab795bdaa05c3c471
MuhammadFurqan777 <furqanfarsi235@gmail.com>
2025-01-12 23:40:53 +0500

    Done

---
+++ average.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float num1, num2, num3, avg;
+	cout<<"Enter Three Values\n";
+	cin>>num1>>num2>>num3; 
+	avg = (num1+num2+num3) /3;
+	cout<<"The average is "<<"num1"<<"num2"<<"num3"  <<avg;
+	
+}
